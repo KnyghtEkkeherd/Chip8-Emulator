b009f4653a2d2e3c4cff7ebc81c8bce7ec2a567b
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2025-06-25 19:36:53 +0200

    init

---
+++ CMakeFiles/chip_emulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip_emulator.dir/link.d"
+  "CMakeFiles/chip_emulator.dir/src/main.c.o"
+  "CMakeFiles/chip_emulator.dir/src/main.c.o.d"
+  "chip_emulator"
+  "chip_emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chip_emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
